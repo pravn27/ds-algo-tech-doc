beb6bbb0e897358fbd4b84c34d0cb2f494d35602
pravn27 <praveen.sri27@gmail.com>
2025-08-09 09:10:09 +0530

    adding DSA challenge platform links

---
+++ docs/platforms/platforms.md
@@ -0,0 +1,9 @@
+---
+sidebar_position: 1
+---
+
+# DSA coding challenge platforms
+
+- [LeetCode](https://leetcode.com/)
+- [Take U Forward](https://takeuforward.org/)
+- [HackerRank](https://www.hackerrank.com/)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,5 @@ import { Redirect } from "@docusaurus/router";
 
 // make default landing page as docs
 export default function Home() {
-  return (
-    <Redirect to="/ds-algo-tech-doc/docs/category/ds--algo-documentations" />
-  );
+  return <Redirect to="/ds-algo-tech-doc/docs/category/documentations" />;
 }
